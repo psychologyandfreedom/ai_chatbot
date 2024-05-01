009754e379ce023e85b8fa6effd49f8745f1e9f9
sunny <matthewnorman.nv@gmail.com>
2024-05-01 16:31:06 -0700

    created frontend directory, built user login form, created signup and login pages...

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,56 @@
+import { AppBar, Toolbar } from '@mui/material'
+import React from 'react'
+import Logo from './shared/Logo'
+import { useAuth } from '../context/AuthContext';
+import NavigationLink from './shared/NavigationLink';
+
+const Header = () => {
+  const auth = useAuth();
+  return (
+    <AppBar sx={{ bgcolor: "transparent", position: "static", boxShadow: "none" }}>
+       <Toolbar sx={{ display: "flex" }}>
+        <Logo />
+        <div>
+          {auth?.isLoggedIn ? (
+          <>
+            <NavigationLink 
+            bg="#00fffc" 
+            to="/chat" 
+            text="Go To Chat" 
+            textColor='black'/>
+
+            <NavigationLink 
+            bg="#51538f" 
+            textColor="white" 
+            to="/"
+            text="Logout" 
+            onClick={auth.logout} 
+            />
+          </>
+          ) : (
+          <>
+          
+          <NavigationLink 
+            bg="#00fffc" 
+            to="/login" 
+            text="Login" 
+            textColor='black'/>
+
+            <NavigationLink 
+            bg="#51538f" 
+            textColor="white" 
+            to="/signup"
+            text="Signup"  
+            />
+          
+          </>
+        )}
+        </div>
+       </Toolbar>
+    </AppBar>
+   
+  )
+}
+
+export default Header
+
